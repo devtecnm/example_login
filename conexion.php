4fc601656b80313001072ed8408d407defcd124b
Wendy Paloma Rodríguez Pedraza <144153217+devtecnm@users.noreply.github.com>
2023-10-16 17:42:28 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,10 @@
 <?php
-    
+    $driver = 'pgsql';
+        $host = getenv('host');
+        $port = getenv('port');
+        $dbname = getenv('dbname');
+        $user =   getenv('user');
+        $password = getenv('password');
     try{
          $conexion = new PDO('mysql:host=localhost;dbname=login_tuto', 'josejaime', 'admin1234');
     }catch(PDOException $prueba_error){
