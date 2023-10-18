5a0ec0caadf4da4bcf41edf664612aeccc787014
Wendy Paloma Rodríguez Pedraza <144153217+devtecnm@users.noreply.github.com>
2023-10-18 12:14:14 -0600

    Create crear_tabla.php

---
+++ crear_tabla.php
@@ -0,0 +1,32 @@
+<?php
+// Datos de conexión a la base de datos PostgreSQL
+$driver = 'pgsql';
+$host = getenv('host');
+$port = getenv('port');
+$dbname = getenv('dbname');
+$user =   getenv('user');
+$password = getenv('password');
+
+try {
+    // Conexión a la base de datos
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+
+    // Consulta SQL para crear una tabla
+    $sql = "CREATE TABLE IF NOT EXISTS login (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    correo varchar(123) NOT NULL,
+    usuario varchar(100) NOT NULL,
+    clave varchar(200) NOT NULL,
+    UNIQUE KEY id (id) )";
+
+    // Ejecutar la consulta SQL
+    $conn->exec($sql);
+
+    echo "Tabla creada con éxito.";
+} catch (PDOException $e) {
+    echo "Error al crear la tabla: " . $e->getMessage();
+}
+
+// Cerrar la conexión a la base de datos
+$conn = null;
+?>
